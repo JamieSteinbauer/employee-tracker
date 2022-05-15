8b03afacfc747db715cdc0f3903dbb7a40625b75
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-14 23:04:50 -0400

    created sql database and tables

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Smith', 1, null)
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, null),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, null),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, null),
+    ('Tom', 'Allen', 8, 7)
+
+INSERT INTO 
