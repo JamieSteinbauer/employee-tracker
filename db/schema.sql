382375da63e34bc30b91dc3acf32e13f4606b7e6
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-14 23:24:48 -0400

    created seeds for tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,26 @@
-CREATE TABLE department (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30)
-)
 
+-- department table --
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+-- role table --
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INTEGER
-)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
+-- employee table --
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
-)
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_ID) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- EMPLOYEE SEEDS --
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Smith', 1, null)
@@ -9,4 +10,23 @@ VALUES
     ('Sarah', 'Lourd', 7, null),
     ('Tom', 'Allen', 8, 7)
 
-INSERT INTO 
+-- ROLE SEEDS --
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Lead Salesperson', 150000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4)
+
+
+-- DEPARTMENT SEEDS --
+INSERT INTO department (name)
+VALUES 
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal")
